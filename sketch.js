60f6029fe4cf0db47fea7d1de38666c7e0842165
GodexPro <adityaagarwal110408@gmail.com>
2021-01-17 12:50:17 +0530

    added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,6 +20,9 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+
+    log6 = new Log(230,180,80,PI/2);
+    
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,6 +40,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    
+
+    chain = new Chain(bird.body,log6.body);
 
 }
 
@@ -61,4 +70,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    chain.display();
+
 }
